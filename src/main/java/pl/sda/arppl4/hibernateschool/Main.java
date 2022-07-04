c2e1108862666396f5a147edf02993a51120b58a
Kasia <kasiakos81@gmail.com>
2022-07-04 21:59:16 +0200

    040722

--- src/main/java/pl/sda/arppl4/hibernateschool/Main.java
+++ src/main/java/pl/sda/arppl4/hibernateschool/Main.java
@@ -1,8 +1,21 @@
 package pl.sda.arppl4.hibernateschool;
 
+import pl.sda.arppl4.hibernateschool.dao.GenericDao;
+import pl.sda.arppl4.hibernateschool.model.Student;
+import pl.sda.arppl4.hibernateschool.parser.StudentCommandLineParser;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        GenericDao<Student> genericStudentDao = new GenericDao<>();
+
+
+        StudentCommandLineParser parser = new StudentCommandLineParser(scanner, genericStudentDao);
+        parser.hellYeah();
 
     }
 }
